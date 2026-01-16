63ca20674356b919ffacf34c616583741ffc2ac8
Даниил Казаков <kazakov.d@sch2009.net>
2026-01-16 22:40:21 +0300

    Добавлен класс Warrior (пока без анимаций и спрайта оружия)

--- src/entities/enemies/__init__.py
+++ src/entities/enemies/__init__.py
@@ -1,2 +1,3 @@
 from .base_enemy import BaseEnemy
 from .base_enemy import BaseEnemy
+from .warrior import Warrior
---
+++ testing.py
@@ -0,0 +1,4 @@
+# Поменяйте base_projectile_test_room на нужный тест если надо
+from tests.manual import base_projectile_test_room as test
+
+test.main()
