6c48a4c09a3276cab0800c4f9d78671bb337a31f
KrasnovAndrey <krasnovandreyyur@mail.ru>
2026-01-23 20:54:19 +0000

    реализовал отображение 10 сердец в GameHUD.display_health

--- src/ui/hud.py
+++ src/ui/hud.py
@@ -1,12 +1,48 @@
 import arcade
 
+from src.core.asset_registries import TexturePaths
+
 
 class GameHUD:
     def __init__(self, window: arcade.Window):
         self.window = window
 
+        self.full_heart_path = TexturePaths.ui_full_heart
+        self.empty_heart_path = TexturePaths.ui_empty_heart
+
+        self.max_hearts = 10
+        self.heart_size = 32
+        self.icon_margin = 8
+
+        self._hearts_sprites = arcade.SpriteList(use_spatial_hash=False)
+
     def display_health(self, value: int):
-        pass
+        if value < 0: # Кто так не делает, тому гореть в аду
+            value = 0
+
+        full_hearts = value // 2
+        if full_hearts > self.max_hearts:
+            full_hearts = self.max_hearts
+
+        start_x = self.icon_margin + self.heart_size / 2
+        y = self.window.height - self.icon_margin - self.heart_size / 2
+
+        self._hearts_sprites.clear()
+
+        for i in range(self.max_hearts):
+            x = start_x + i * (self.heart_size + 2)
+
+            if i < full_hearts:
+                texture_path = self.full_heart_path
+            else:
+                texture_path = self.empty_heart_path
+
+            sprite = arcade.Sprite(texture_path)
+            sprite.center_x = x
+            sprite.center_y = y
+            self._hearts_sprites.append(sprite)
+
+        self._hearts_sprites.draw()
 
     def display_mana(self, value: int):
         pass
