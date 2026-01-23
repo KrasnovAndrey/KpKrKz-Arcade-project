4f90f50487ced4b0927f9829b818185ee4eec586
KrasnovAndrey <krasnovandreyyur@mail.ru>
2026-01-23 20:37:13 +0000

    добавил каркас GameHUD с методами отображения здоровья, маны и валюты

---
+++ src/ui/hud.py
@@ -0,0 +1,15 @@
+import arcade
+
+
+class GameHUD:
+    def __init__(self, window: arcade.Window):
+        self.window = window
+
+    def display_health(self, value: int):
+        pass
+
+    def display_mana(self, value: int):
+        pass
+
+    def display_currency(self, value: int):
+        pass
