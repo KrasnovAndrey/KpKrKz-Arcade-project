26d207db12df9f56b7e2f69215ac49830b56da78
KrasnovAndrey <krasnovandreyyur@mail.ru>
2026-01-23 21:20:59 +0000

    реализовал отображение маны в GameHUD.display_mana

--- src/ui/hud.py
+++ src/ui/hud.py
@@ -10,11 +10,18 @@ class GameHUD:
         self.full_heart_path = TexturePaths.ui_full_heart
         self.empty_heart_path = TexturePaths.ui_empty_heart
 
+        self.full_mana_path = TexturePaths.ui_full_mana_ball
+        self.empty_mana_path = TexturePaths.ui_empty_mana_ball
+
         self.max_hearts = 10
         self.heart_size = 32
         self.icon_margin = 8
 
+        self.max_mana = 4
+        self.mana_size = 24
+
         self._hearts_sprites = arcade.SpriteList(use_spatial_hash=False)
+        self._mana_sprites = arcade.SpriteList(use_spatial_hash=False)
 
     def display_health(self, value: int):
         if value < 0: # Кто так не делает, тому гореть в аду
@@ -48,7 +55,26 @@ class GameHUD:
         self._hearts_sprites.draw()
 
     def display_mana(self, value: int):
-        pass
+        if value < 0:
+            value = 0
+        if value > self.max_mana:
+            value = self.max_mana
+
+        start_x = self.icon_margin + self.mana_size / 2
+        y = self.window.height - self.icon_margin - self.heart_size - self.icon_margin - self.mana_size / 2
+
+        self._mana_sprites.clear()
+
+        for i in range(self.max_mana):
+            x = start_x + i * (self.mana_size + 4)
+            texture_path = self.full_mana_path if i < value else self.empty_mana_path
+
+            sprite = arcade.Sprite(texture_path)
+            sprite.center_x = x
+            sprite.center_y = y
+            self._mana_sprites.append(sprite)
+
+        self._mana_sprites.draw()
 
     def display_currency(self, value: int):
         pass
